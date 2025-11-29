6dbf51b60ddc312d9b26959021e2e4506f9b37e9
thesleepyboy <matiasezequielyanes@gmail.com>
2025-11-29 12:41:49 -0300

    Add: implement HTTPS agent with certificate for Axios requests in BaseClient

--- src/clients/baseClient.ts
+++ src/clients/baseClient.ts
@@ -1,10 +1,20 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url"
+import https from "https";
 import axios from "axios"
+
 import type { AxiosInstance } from "axios";
 import HttpException from "./httpException.js";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default abstract class BaseClient {
     protected readonly axiosInstance: AxiosInstance;
     private readonly baseUrl: string = "https://api.bcra.gob.ar/";
+    private readonly httpsAgent = new https.Agent({
+        ca: fs.readFileSync(path.join(__dirname, "..", "cert", "bcra.gob.ar.crt"))
+    });
 
     constructor() {
         this.axiosInstance = axios.create({
@@ -18,7 +28,7 @@ export default abstract class BaseClient {
         queryParams?: TParams
     ): Promise<TResponse> {
         try {
-            const response = await this.axiosInstance.get(endpoint, { params: queryParams });
+            const response = await this.axiosInstance.get(endpoint, { params: queryParams, httpsAgent: this.httpsAgent });
 
             return response.data;
 
@@ -27,7 +37,7 @@ export default abstract class BaseClient {
             throw e;
         }
     }
-    private buildErrorResponse(error: unknown): HttpException | Error {
+    private buildErrorResponse(error: unknown): HttpException {
         if (axios.isAxiosError(error) && error.response) {
             return new HttpException(
                 error.response,
@@ -37,7 +47,7 @@ export default abstract class BaseClient {
         }
 
         if (error instanceof Error) {
-            return new Error(error.message);
+            return new HttpException(error.message);
         }
 
         return new HttpException("Unknown error.", 500, { cause: error });
