f1579dd8f065df615089bc6aeca12d025448bed5
thesleepyboy <matiasezequielyanes@gmail.com>
2025-11-29 12:56:25 -0300

    fix: update import paths for monetarias and parameters in index.ts

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,12 +1,12 @@
 export * from "./apiResponse.js";
 
 //#region Principales variables
-export * from "./principalesVariables/monetarias.js";
-export * from "./principalesVariables/datosMonetaria.js";
-export * from "./principalesVariables/metodologia.js";
+export * from "./monetarias/monetarias.js";
+export * from "./monetarias/datosMonetaria.js";
+export * from "./monetarias/metodologia.js";
 
-export * from "./principalesVariables/parameters/monetariasParameters.js";
-export * from "./principalesVariables/parameters/datosMonetariaParameters.js";
-export * from "./principalesVariables/parameters/metodologiaParameters.js";
+export * from "./monetarias/parameters/monetariasParameters.js";
+export * from "./monetarias/parameters/datosMonetariaParameters.js";
+export * from "./monetarias/parameters/metodologiaParameters.js";
 //#endregion
 
